972790b643ffd155ff4efb0772eddef654c9d930
elgeor <elgeor.sk@gmail.com>
2023-01-03 12:05:30 +0200

    feat: Add E2E Tranfer Funds test scenario

--- cypress/e2e/s4_E2E_Testing/tranfer-funds.cy.js
+++ cypress/e2e/s4_E2E_Testing/tranfer-funds.cy.js
@@ -10,10 +10,48 @@ describe('E2E Tranfer Funds test scenario', { testIsolation:false }, () => {
     })
 
     it('should fill tranfer funds form', () => {
-        
+        cy.contains('Transfer Funds').click()
+        cy.get('#tf_fromAccountId').select(3)
+        cy.get('#tf_fromAccountId').should('have.value', 4) // Loan(Avail. balance = $ 780)
+        cy.get('#tf_toAccountId').select(1)
+        cy.get('#tf_toAccountId').should('have.value', 2) // Checking(Avail. balance = $ -500.2)
+        cy.get('#tf_amount').type('1000')
+        cy.get('#tf_description').type('tranfer fund description')
+
+        cy.get('#btn_submit').click()
     })
 
     it('should verify correct data', () => {
+        cy.url().should('include', 'transfer-funds-verify.html')
+
+        cy.get('#tf_fromAccountId').should('have.value', 'Loan')
+        cy.get('#tf_toAccountId').should('have.value', 'Checking')
+        cy.get('#tf_amount').should('have.value','1000')
+        cy.get('#tf_description').should('have.value', 'tranfer fund description')
+
+        cy.get('#btn_submit').click()
+    })
+
+    it('should confirm the transfer of funds', () => {
+        cy.url().should('include', 'transfer-funds-confirm.html')
+
+        cy.get('.alert-success').should('be.visible').and('contain', 'You successfully submitted your transaction')
+        cy.xpath('//div[@class="board-content"]//div[@class="span3"]').should('have.length', 3)
+        cy.xpath('(//div[@class="board-content"]//div[@class="span3"])').should($element => {
+            expect($element[0].outerText).to.contain('Loan')
+        })
+
+        cy.xpath('(//div[@class="board-content"]//div[@class="span3"])').should($element => {
+            expect($element[1].outerText).to.contain('Checking')
+        })
         
+        cy.xpath('(//div[@class="board-content"]//div[@class="span3"])').should($element => {
+            expect($element[2].outerText).to.contain('1000')
+        })
+    })
+
+    it('should logout from application', () => {
+        cy.logout()
+        cy.url().should('include', 'index.html')
     })
 })
